863f7a1db5a3cdbe7c50d10c9630688991d70850
NomuraKohei <seiryu2nd@gmail.com>
2022-08-27 19:15:51 +0900

    feat: create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next/types';
+import Layout from 'src/components/Layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="Seawater Temperature Data Visualization">
+      a
+    </Layout>
+  );
+}
+
+export default Home
