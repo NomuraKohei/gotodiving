578e22236686bffee858858707b763724366c47e
NomuraKohei <seiryu2nd@gmail.com>
2022-08-28 18:42:01 +0900

    fix: token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NextPage } from 'next/types';
 import Layout from 'src/components/Layout';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
     <Layout title="Seawater Temperature Data Visualization">
-      a
+      <div className={styles.main}>
+        a
+      </div>
     </Layout>
   );
 }
