cd6a8111ea1022f57f44e11ad4cd08d4b4913f3f
NomuraKohei <seiryu2nd@gmail.com>
2022-08-30 02:59:27 +0900

    feat: topのヒーローヘッダー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,38 @@ import { NextPage } from "next/types";
 import Layout from "src/components/Layout";
 import styles from "@styles/Home.module.scss";
 import Image from "next/image";
+import iconLandscape from "@images/icon-landscape.webp";
+import iconMarineLife from "@images/icon-marinelife.webp";
+import iconEquipment from "@images/icon-equipment.webp";
+import iconGourmet from "@images/icon-gourmet.webp";
+import bg from "@images/background-top.webp";
+import iconArrow from "@images/icon-arrow.webp";
 
 const Home: NextPage = () => {
+  const images = [
+    { imageData: iconLandscape, alt: "風景のアイコン" },
+    { imageData: iconMarineLife, alt: "生物のアイコン" },
+    { imageData: iconEquipment, alt: "装備のアイコン" },
+    { imageData: iconGourmet, alt: "グルメのアイコン" },
+  ];
   return (
     <Layout title="トップ">
+      <div className={styles.backgroundWrapper}>
+        <Image alt="魔王の宮殿の画像" src={bg} layout="fill" objectFit="cover" quality={100} />
+      </div>
       <section className={styles.heroheader}>
-        <h1>海の歩き方</h1>
-        <p>スキューバダイビングで海を楽しもう</p>
-        <div>
-          <Image src="" alt="" />
-          <Image src="" alt="" />
-          <Image src="" alt="" />
-          <Image src="" alt="" />
+        <h1 className={styles.title}>海の歩き方</h1>
+        <p className={styles.subTitle}>スキューバダイビングで海を楽しもう</p>
+        <div className={styles.icons}>
+          {images.map((item, index) => (
+            <div className={styles.icon} key={index}>
+              <Image src={item.imageData} alt={item.alt} width={148} height={148} />
+            </div>
+          ))}
         </div>
-        <div>
-          <Image src="" alt="" />
-          know more
+        <div className={styles.arrow}>
+          <Image src={iconArrow} alt="矢印アイコン" width={8} height={40} />
+          {/* know more */}
         </div>
       </section>
       <p>ダイビングの楽しみ方は人ぞれぞれ</p>
